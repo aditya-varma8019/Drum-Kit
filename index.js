970ac31cfc759fcf7a7294ca504e40005520eb66
aditya-varma8019 <116490912+aditya-varma8019@users.noreply.github.com>
2023-03-17 21:48:40 +0530

    Drum kit files including images, sounds, frontHand

---
+++ index.js
@@ -0,0 +1,93 @@
+var audio;
+
+
+for(var i=0;i<7;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+    {
+        var buttonInnerHTML = this.innerHTML;
+        //
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio=new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio=new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio=new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;    
+            case "d":
+                var audio=new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break; 
+            case "j":
+                var audio=new Audio("sounds/snare.mp3");
+                audio.play();
+                break;        
+            case "k":
+                var audio=new Audio("sounds/crash.mp3");
+                audio.play();
+                break; 
+            case "l":
+                var audio=new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;         
+            default:
+                break;
+        }
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keypress",function (event) 
+{
+        switch (event.key) {
+            case "w":
+                var audio=new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio=new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio=new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;    
+            case "d":
+                var audio=new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break; 
+            case "j":
+                var audio=new Audio("sounds/snare.mp3");
+                audio.play();
+                break;        
+            case "k":
+                var audio=new Audio("sounds/crash.mp3");
+                audio.play();
+                break; 
+            case "l":
+                var audio=new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;         
+            default:
+                break;
+        }
+        buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey)
+{
+    var buttonPressed = document.querySelector("." + currentKey);
+    buttonPressed.classList.toggle("pressed");
+
+    setTimeout(function ()
+    {
+        buttonPressed.classList.remove("pressed");
+    },100);
+}
+
